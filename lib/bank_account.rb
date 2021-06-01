e40069efed909fc33450f2d7c624aafe691f5155
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-01 11:14:26 +0100

    Refactored to attr reader

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount
-
+  attr_reader :balance 
   def initialize
     @balance = 0
   end
@@ -7,8 +7,4 @@ class BankAccount
   def deposit(amount)
     @balance += amount
   end
-
-  def balance
-    @balance
-  end 
 end
