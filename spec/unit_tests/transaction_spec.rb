513eb8b639afa54376538f7890a3b9f2efbb6b94
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-02 17:35:24 +0100

    Removed BankAccount instance from Transaction spec test as per feedback

--- spec/unit_tests/transaction_spec.rb
+++ spec/unit_tests/transaction_spec.rb
@@ -3,15 +3,11 @@
 require 'transaction'
 
 describe Transaction do
-  let(:account) { double BankAccount.new }
   subject(:transaction) do
-    described_class.new(account.deposit, account.withdraw, account.balance)
+    described_class.new(150, 0, 150)
   end
 
   it 'stores the relevant info about each transaction' do
-    allow(account).to receive(:deposit).and_return(150)
-    allow(account).to receive(:withdraw).and_return(0)
-    allow(account).to receive(:balance).and_return(150)
     expect(transaction.credit).to eq 150
     expect(transaction.debit).to eq 0
     expect(transaction.current_balance).to eq 150
