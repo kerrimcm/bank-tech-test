5b07c7788596113b947b1f693cf46a28b4ece1f4
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-02 14:24:20 +0100

    Added context blocks to bank_account_spec

--- spec/unit_tests/bank_account_spec.rb
+++ spec/unit_tests/bank_account_spec.rb
@@ -10,34 +10,38 @@ describe BankAccount do
   let(:transaction) { double(:transaction) }
   let(:printed_statement) { double(:printed_statement) }
 
-  it 'allows the user to make a desposit' do
-    subject.deposit(1)
-    expect(subject.balance).to eq 1
+  context '#deposit' do
+    it 'allows the user to make a desposit' do
+      subject.deposit(1)
+      expect(subject.balance).to eq 1
+    end
+
+    it 'allows the user to top up this balance further' do
+      subject.deposit(1)
+      subject.deposit(10)
+      expect(subject.balance).to eq 11
+    end
+
+    it 'throws an error if I try to deposit a negative amount' do
+      expect { subject.deposit(-300) }. to raise_error 'You may not deposit negative amounts'
+    end
   end
 
-  it 'allows the user to top up this balance further' do
-    subject.deposit(1)
-    subject.deposit(10)
-    expect(subject.balance).to eq 11
-  end
+  context '#withdraw' do
+    it 'allows the user to withdraw from their account' do
+      subject.deposit(100)
+      subject.withdraw(50)
+      expect(subject.balance).to eq 50
+    end
 
-  it 'allows the user to withdraw from their account' do
-    subject.deposit(100)
-    subject.withdraw(50)
-    expect(subject.balance).to eq 50
-  end
+    it 'throws an error if I try to withdraw a negative amount' do
+      subject.deposit(1000)
+      expect { subject.withdraw(-300) }. to raise_error 'You may not withdraw negative amounts'
+    end
 
-  it 'throws an error if I try to deposit a negative amount' do
-    expect { subject.deposit(-300) }. to raise_error 'You may not deposit negative amounts'
-  end
-
-  it 'throws an error if I try to withdraw a negative amount' do
-    subject.deposit(1000)
-    expect { subject.withdraw(-300) }. to raise_error 'You may not withdraw negative amounts'
-  end
-
-  it 'throws an error if I try to withdraw when there is 0 in the account' do
-    expect { subject.withdraw(300) }. to raise_error 'Your balance is currently £0.00'
+    it 'throws an error if I try to withdraw when there is 0 in the account' do
+      expect { subject.withdraw(300) }. to raise_error 'Your balance is currently £0.00'
+    end
   end
 
   it 'records the transactions of the customer' do
@@ -45,8 +49,10 @@ describe BankAccount do
     expect(bank_account.transactions).to include transaction
   end
 
-  it 'prints out a summary of transactions' do
-    expect(printed_statement).to receive(:print)
-    bank_account.summary
+  context '#summary' do
+    it 'prints out a summary of transactions' do
+      expect(printed_statement).to receive(:print)
+      bank_account.summary
+    end
   end
 end
