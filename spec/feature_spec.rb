6ea66f4787a3b3ada82132db0440a7e667a7bfe4
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-02 12:10:53 +0100

    Deposit feature now tested

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -13,4 +13,15 @@ describe 'Bank account features' do
       expect { @new_account.summary }.to output(bank_statement).to_stdout
     end
   end
+
+  context '#withdraw' do
+    it 'allows the user to withdraw money from their account' do
+      @new_account.deposit(1000)
+      @new_account.withdraw(500)
+      bank_statement = "date || credit || debit || balance\n"\
+                  "02/06/2021 ||  || 500.00 || 500.00\n"\
+                  "02/06/2021 || 1000.00 ||  || 1000.00\n"
+      expect { @new_account.summary }.to output(bank_statement).to_stdout
+    end
+  end
 end
