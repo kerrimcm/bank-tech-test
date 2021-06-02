6c0a3eafafb08a7ac19c98883ff5283f4870ce60
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-02 12:38:45 +0100

    Summary feature now tested

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -24,4 +24,17 @@ describe 'Bank account features' do
       expect { @new_account.summary }.to output(bank_statement).to_stdout
     end
   end
+
+  context '#summary' do
+    it 'prints the past several transactions in reverse chronological order in a statement' do
+      @new_account.deposit(1000)
+      @new_account.withdraw(500)
+      @new_account.deposit(2000)
+      bank_statement = "date || credit || debit || balance\n"\
+                       "02/06/2021 || 2000.00 ||  || 2500.00\n"\
+                       "02/06/2021 ||  || 500.00 || 500.00\n"\
+                       "02/06/2021 || 1000.00 ||  || 1000.00\n"
+      expect { @new_account.summary }.to output(bank_statement).to_stdout
+    end
+  end
 end
