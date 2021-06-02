1c882d8ccc63601c226adce348831a08449aba17
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-02 12:41:05 +0100

    Refactored tests to generalise the days date

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,13 +3,14 @@ require 'bank_account'
 describe 'Bank account features' do
   before(:example) do
     @new_account = BankAccount.new
+    @current_date = Time.new.strftime('%d/%m/%Y')
   end
 
   context '#deposit' do
     it 'allows the user to deposit money into their account' do
       @new_account.deposit(200)
       bank_statement =  "date || credit || debit || balance\n"\
-                        "02/06/2021 || 200.00 ||  || 200.00\n"
+                        "#{@current_date} || 200.00 ||  || 200.00\n"
       expect { @new_account.summary }.to output(bank_statement).to_stdout
     end
   end
@@ -19,8 +20,8 @@ describe 'Bank account features' do
       @new_account.deposit(1000)
       @new_account.withdraw(500)
       bank_statement = "date || credit || debit || balance\n"\
-                  "02/06/2021 ||  || 500.00 || 500.00\n"\
-                  "02/06/2021 || 1000.00 ||  || 1000.00\n"
+                       "#{@current_date} ||  || 500.00 || 500.00\n"\
+                       "#{@current_date} || 1000.00 ||  || 1000.00\n"
       expect { @new_account.summary }.to output(bank_statement).to_stdout
     end
   end
@@ -31,9 +32,9 @@ describe 'Bank account features' do
       @new_account.withdraw(500)
       @new_account.deposit(2000)
       bank_statement = "date || credit || debit || balance\n"\
-                       "02/06/2021 || 2000.00 ||  || 2500.00\n"\
-                       "02/06/2021 ||  || 500.00 || 500.00\n"\
-                       "02/06/2021 || 1000.00 ||  || 1000.00\n"
+                       "#{@current_date} || 2000.00 ||  || 2500.00\n"\
+                       "#{@current_date} ||  || 500.00 || 500.00\n"\
+                       "#{@current_date} || 1000.00 ||  || 1000.00\n"
       expect { @new_account.summary }.to output(bank_statement).to_stdout
     end
   end
